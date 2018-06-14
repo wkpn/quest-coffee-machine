39eb9ff4f576cff427f0b69a46a1cdf228057384
wkpn <yegornosov@gmail.com>
2018-06-15 01:10:48 +0300

    rename folder from saved_data to labels

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 FACE_RECOGNITION_MODEL = './models/dlib_face_recognition_resnet_model_v1.dat'
 SHAPE_PREDICTOR_MODEL = './models/shape_predictor_68_face_landmarks.dat'
 TRAIN_DATA_FOLDER = './train_data'
-LABELS = './saved_data/labels.dat'
+LABELS = './labels/labels.dat'
 
 ANONYMOUS_UPN = 'YOUR ANONYMOUS UPN HERE'
 REST_API_URL = 'PUT YOUR URL HERE'
