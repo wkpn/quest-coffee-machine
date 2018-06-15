f7fdfec37c2a7320be81db69f8925432d01c111c
yegor <yegornosov@gmail.com>
2018-06-15 15:58:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,17 +16,16 @@ if __name__ == '__main__':
     def callback(channel):
         result = recognizer.face_recognition()
 
-        if not result:
-            for upn in result:
-                data = [{
-                    "timestamp": int(time.time()),
-                    "upn": upn,
-                    "action": "Made a cup of coffee",
-                    "message": "Thanks!"
-                }]
+        for upn in result:
+            data = [{
+                "timestamp": int(time.time()),
+                "upn": upn,
+                "action": "Made a cup of coffee",
+                "message": "Thanks!"
+            }]
 
-                requests.post(REST_API_URL, json=data)
-
+            response = requests.post(REST_API_URL, json=data)
+            print(upn, response.status_code)
 
     GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
     GPIO.add_event_callback(channel, callback)
